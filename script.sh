9d4e473829ff7a7971062558cc6eaea08fab0e04
Tiago Rios <tiagojsrios@gmail.com>
2022-05-02 23:22:45 +0200

    Changed default path from prepare-commit-msg hook

--- script.sh
+++ script.sh
@@ -8,16 +8,22 @@ else
     DIRECTORY=$1
 fi
 
+if [ -f "~/.git-templates/hooks/prepare-commit-msg" ]
+then
+    echo "Couldn't find ~/.git-templates/hooks/prepare-commit-msg file"
+    exit
+fi
+
 for dir in $DIRECTORY
 do
     for subdir in "$dir"/*
     do
         HOOKS="$subdir/.git"
         
-        if [[ -d "$HOOKS/hooks" ]]
+        if [ -d "$HOOKS/hooks" ]
         then
-            cp ./prepare-commit-msg $HOOKS/hooks
-            printf "Copied prepare-commit-msg file to $HOOKS/hooks\n"
+            cp ~/.git-templates/hooks/prepare-commit-msg $HOOKS/hooks
+            echo "Copied prepare-commit-msg file to $HOOKS/hooks"
         fi
     done
 done
